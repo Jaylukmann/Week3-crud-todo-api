74827085dc9ee0bd946c478284fe6b756927acda
Jimoh Lukman <jaylukmann@gmail.com>
2026-07-26 16:34:32 +0100

    Corrected the _id issue

--- public/script.js
+++ public/script.js
@@ -1,22 +1,18 @@
-// const API = "http://localhost:5000/todos";
 const API = "https://week3-crud-todo-api-f24x.onrender.com/todos";
 
 const list = document.getElementById("todoList");
 const form = document.getElementById("todoForm");
 
 async function loadTodos() {
-
     const res = await fetch(API);
-
     const todos = await res.json();
 
     list.innerHTML = "";
 
     todos.forEach(todo => {
-
         const li = document.createElement("li");
 
-        if(todo.completed){
+        if (todo.completed) {
             li.classList.add("completed");
         }
 
@@ -24,80 +20,60 @@ async function loadTodos() {
             <span>${todo.task}</span>
 
             <div>
-
-                <button onclick="toggleTodo(${todo.id}, ${todo.completed})">
+                <button onclick="toggleTodo('${todo._id}', ${todo.completed})">
                     ${todo.completed ? "Undo" : "Complete"}
                 </button>
 
-                <button onclick="deleteTodo(${todo.id})">
+                <button onclick="deleteTodo('${todo._id}')">
                     Delete
                 </button>
-
             </div>
         `;
 
         list.appendChild(li);
-
     });
-
 }
 
-form.addEventListener("submit", async (e)=>{
-
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const task = document.getElementById("task").value;
 
-    await fetch(API,{
-
-        method:"POST",
-
-        headers:{
-            "Content-Type":"application/json"
+    await fetch(API, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
         },
-
-        body:JSON.stringify({
+        body: JSON.stringify({
             task,
-            completed:false
+            completed: false
         })
-
     });
 
     form.reset();
-
     loadTodos();
-
 });
 
-async function toggleTodo(id,current){
-
-    await fetch(`${API}/${id}`,{
-
-        method:"PATCH",
-
-        headers:{
-            "Content-Type":"application/json"
+async function toggleTodo(id, current) {
+    await fetch(`${API}/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
         },
-
-        body:JSON.stringify({
-            completed:!current
+        body: JSON.stringify({
+            completed: !current
         })
-
     });
 
     loadTodos();
-
 }
 
-async function deleteTodo(id){
-
-    await fetch(`${API}/${id}`,{
-
-        method:"DELETE"
+async function deleteTodo(id) {
+    await fetch(`${API}/${id}`, {
+        method: "DELETE"
     });
 
     loadTodos();
-
 }
 
 loadTodos();
